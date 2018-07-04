e8b574ff7e98e1fce477799c67f00aa5caecb2e1
Eduardo Alencar de Oliveira <ealencar10@gmail.com>
2018-07-03 23:05:46 -0300

    Employee manager admin
    Employee manager app modifiable via admin

--- vagaped/employeemanager/models.py
+++ vagaped/employeemanager/models.py
@@ -3,11 +3,14 @@ from django.db import models
 # Create your models here.
 class Department(models.Model):
     title = models.CharField(max_length=200)
-
+    
+    def __str__(self):
+        return self.title
 
 class Employee(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField()
     department = models.ForeignKey(Department,
                                    on_delete=models.PROTECT)
-
+    def __str__(self):
+        return  self.name
